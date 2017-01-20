3df2d6574a8c95e757da67f14e2755a85fb0e412
Peter Desmet <peter.desmet.work@gmail.com>
2017-01-21 00:43:53 +0100

    Keep tooltip in div with position relative on selector
    Before, the (absolute) tooltip was not contained within the selector
    div. Test by setting ` style="margin-top: 200px”` on `<div
    class="container">` in `example/index.html`. Now fixed by setting
    position: relative on selector.
    Side effect was that tooltip came closer to selected cell, so updated
    position.

--- src/calendar-heatmap.js
+++ src/calendar-heatmap.js
@@ -84,6 +84,7 @@ function calendarHeatmap() {
 
     function drawChart() {
       var svg = d3.select(chart.selector())
+        .style('position', 'relative')
         .append('svg')
         .attr('width', width)
         .attr('class', 'calendar-heatmap')
@@ -122,7 +123,7 @@ function calendarHeatmap() {
             .html(tooltipHTMLForDate(d))
             .style('left', function () { return Math.floor(i / 7) * SQUARE_LENGTH + 'px'; })
             .style('top', function () {
-              return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 3 + 'px';
+              return formatWeekday(d.getDay()) * (SQUARE_LENGTH + SQUARE_PADDING) + MONTH_LABEL_PADDING * 2 + 'px';
             });
         })
         .on('mouseout', function (d, i) {
